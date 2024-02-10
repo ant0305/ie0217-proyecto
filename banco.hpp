1719e296ece2cf18ff4d8b205e6f26c0a5de1ee1
Anthony Calvo <anthonycalvo50@gmail.com>
2024-02-10 16:49:43 -0600

    Agregando parte del codigo necesario para la implementacion del proyecto

---
+++ banco.hpp
@@ -0,0 +1,71 @@
+#ifndef BANCO_HPP
+#define BANCO_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <set>
+#include <limits>
+
+class Cliente;
+
+class CuentaBancaria {
+public:
+    CuentaBancaria(std::string titular, std::string numeroCuenta, std::string moneda, double saldoInicial = 0.0)
+    : titular(titular), numeroCuenta(numeroCuenta), saldo(saldoInicial), moneda(moneda) {}
+
+protected:
+    std::string titular;
+    std::string numeroCuenta;
+    double saldo;
+    std::string moneda;
+};
+
+class CDP {
+private:
+    std::string titular;
+    double monto;
+    double tasaInteres;
+    int plazoDias;
+    std::string moneda;
+
+public:
+    CDP(std::string titular, double monto, double tasaInteres, int plazoDias, std::string moneda)
+    : titular(titular), monto(monto), tasaInteres(tasaInteres), plazoDias(plazoDias), moneda(moneda) {}
+
+    static void crearYAgregarCDPParaCliente(std::vector<Cliente*>& clientes);
+};
+
+
+class Cliente {
+private:
+    static std::set<int> idsAsignados;
+    int id;
+    std::string nombre;
+    CuentaBancaria* cuentaColones;
+    CuentaBancaria* cuentaDolares;
+    CDP* cdp;
+
+public:
+
+    Cliente(int id, std::string nombre);
+
+    static bool asignarID(int id);
+
+    int obtenerID() const;
+    std::string obtenerNombre() const;
+
+    static bool esNumeroValido(const std::string& str, int& numero);
+
+    void agregarCDP(const CDP &nuevoCDP);
+    std::vector<CDP> cdps;
+
+    void agregarCuentaColones(CuentaBancaria* cuenta);
+    void agregarCuentaDolares(CuentaBancaria* cuenta);
+    static void agregarCuentaABanco(std::vector<Cliente*>& clientes);
+    ~Cliente();
+
+};
+
+#endif
