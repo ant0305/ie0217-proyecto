cf2dd446e100c098a67ff9f413709d299e5b8faa
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2024-02-10 18:29:49 -0600

    Agregando avances al main

---
+++ src/funciones.hpp
@@ -0,0 +1,20 @@
+#ifndef FUNCIONES_HPP
+#define FUNCIONES_HPP
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+struct Cliente
+{
+    int Cedula;
+    
+};
+
+
+
+void MostrarMenu();
+
+void procesarOpcion(Cliente& cliente);
+
+
+#endif
