59f33b55e341bdfd74f7e97c8379c7c667aafa78
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-02-12 03:44:49 -0600

    Se le agrego la funcion para que se imprima la tabla con las cuotas, intereses y montos restantes del prestamo
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- info.cpp
+++ info.cpp
@@ -1,4 +1,7 @@
 #include "info.hpp"
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 void MostrarMenuSecundario (){
@@ -6,7 +9,7 @@ void MostrarMenuSecundario (){
     cout << "\n 1. Informacion Prestamo Personal\n";
     cout << "\n 2. Informacion Prestamo Prendario\n";
     cout << "\n 3. Informacion Prestamo Hipotecario\n";
-    cout << "\n 4. Salir";
+    cout << "\n 4. Salir\n";
 }
 
 void InfoPrestamoPersonal(){
@@ -26,6 +29,82 @@ void InfoPrestamoPrendario() {
 
 void InfoPrestamoHipotecario() {
     cout << "\n ---Informacion sobre Prestamos Hipotecarios---\n";
-    cout << "El prestamo hipotecario es un prestamo con garantia hipotecaria, es decir, un inmueble es utilizado como garantia, estos creditos son adquiridos para la compra de lotes, viviendas, entre otros.";
-    cout << "Por tanto, el bien adquirido queda hipotecado a favor de la entiendad financiera hasta que finalice el pago del prestamo.";
+    cout << "El prestamo hipotecario es un prestamo con garantia hipotecaria, es decir, un inmueble es utilizado como garantia, estos creditos son adquiridos para la compra de lotes, viviendas, entre otros.\n";
+    cout << "Por tanto, el bien adquirido queda hipotecado a favor de la entiendad financiera hasta que finalice el pago del prestamo.\n";
+}
+
+void Prestamo::calcularPrestamo(){
+    std::cout << "\nSeleccione la moneda (1 para Colones, 2 para Dolares): ";
+    int monedaOpcion;
+    std::cin >> monedaOpcion;
+    std::string moneda = (monedaOpcion == 1) ? "Colones" : "Dolares";
+
+    std::cout << "\nIngrese el monto del prestamo: ";
+    double monto;
+    std::cin >> monto;
+
+    int plazoMeses;
+    double tasaInteres;
+    if (monedaOpcion == 1) {
+        std::cout << "\nOpciones de prestamo en Colones:\n"
+                  << "1. 144 meses con 12.72% de interes\n"
+                  << "2. 108 meses con 25.44% de interes\n"
+                  << "3. 84 meses con 38.16% de interes\n"
+                  << "4. Volver al menu\n";
+    } else {
+        std::cout << "\nOpciones de prestamo en Dolares:\n"
+                  << "1. 144 meses con 10.09% de interes\n"
+                  << "2. 108 meses con 20.18% de interes\n"
+                  << "3. 84 meses con 30.27% de interes\n"
+                  << "4. Volver al menu\n";
+    }
+
+    std::cout << "Seleccione una opcion: ";
+    int prestamoOpcion;
+    std::cin >> prestamoOpcion;
+
+    switch (prestamoOpcion) {
+        case 1:
+            plazoMeses = 144;
+            tasaInteres = (monedaOpcion == 1) ? 12.72 : 10.09;
+            break;
+        case 2:
+            plazoMeses = 108;
+            tasaInteres = (monedaOpcion == 1) ? 25.44 : 20.18;
+            break;
+        case 3:
+            plazoMeses = 84;
+            tasaInteres = (monedaOpcion == 1) ? 38.16 : 30.27;
+            break;
+        case 4:
+            return;
+        default:
+            std::cerr << "\nOpcion no valida, seleccionando opcion predeterminada.\n";
+            plazoMeses = 144;
+            tasaInteres = (monedaOpcion == 1) ? 12.72 : 10.09;
+            break;
+    }
+
+    // calcula la cuota mensual
+    double tasaInteresMensual = tasaInteres / 100 / 12;
+    double cuotaMensual = monto * tasaInteresMensual / (1 - std::pow(1 + tasaInteresMensual, -plazoMeses));
+
+    // tabla
+    std::cout << std::setw(10) << std::left << "Mes"
+              << std::setw(15) << std::left << "Cuota"
+              << std::setw(15) << std::left << "Intereses"
+              << std::setw(20) << std::left << "Pago sin Intereses"
+              << std::setw(20) << std::left << "Monto Restante\n";
+
+    double montoRestante = monto;
+    for (int mes = 1; mes <= plazoMeses; ++mes) {
+        double intereses = montoRestante * tasaInteresMensual;
+        double amortizacion = cuotaMensual - intereses;
+        montoRestante -= amortizacion;
+        std::cout << std::setw(10) << mes
+                  << std::setw(15) << std::fixed << std::setprecision(2) << cuotaMensual
+                  << std::setw(15) << std::fixed << std::setprecision(2) << intereses
+                  << std::setw(20) << std::fixed << std::setprecision(2) << amortizacion
+                  << std::fixed << std::setprecision(2) << std::setw(20) << montoRestante << "\n";
+    }
 }
