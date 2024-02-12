42d9d5879ccc9b94994e9f67901a2e0a2f879596
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2024-02-11 22:09:53 -0600

    Agregando a la implementacion final

--- src/funciones.cpp
+++ src/funciones.cpp
@@ -1,38 +1,79 @@
 #include "funciones.hpp"
+#include "info.hpp"
 #include <iostream>
 #include <limits>
 #include <cstdlib>
 #include <cmath>
 
-void MostrarMenu (){ //Esta funcion ejecuta el menu.
+void MostrarMenu() {
     std::cout << "\n ---BIENVENIDO AL BANCO---\n";
     std::cout << "\n 1. Seleccione esta opcion si quiere consultar informacion\n";
-    std::cout << "\n 2. Seleccine esta opcion si desea realizar operaciones en su cuenta.\n";
+    std::cout << "\n 2. Seleccione esta opcion si desea realizar operaciones en su cuenta.\n";
     std::cout << "\n 3. Salir\n";
 }
 
-void procesarOpcion(Cliente& cliente){
-    
+void procesarOpcion(Cliente& cliente) {
     int opcion;
-    do{
-        try{
-        std::cout<< "Ingrese una opcion: "; 
-        std::cin>>opcion;
+    do {
+        try {
+            std::cout << "Ingrese una opcion: ";
+            std::cin >> opcion;
 
-        if (std::cin.fail()){
-            throw std::runtime_error("Entrada no valida. Por favor, ingrese un numero.");
+            if (std::cin.fail()) {
+                throw std::runtime_error("Entrada no valida. Por favor, ingrese un numero.");
             }
 
-        switch(opcion){
-                case 1:
-                //Modo_Informacion();
+            switch (opcion) {
+                case 1: {
+                    while (true) {
+                        MostrarMenuSecundario();
+                        int opcion_secundario;
+                        std::cout << "Ingrese una opcion para ver otra informacion disponible, o bien, presione 4 para volver al menu principal: ";
+                        std::cin >> opcion_secundario;
+
+                        if (std::cin.fail()) {
+                            throw std::runtime_error("Opcion no valida. Por favor, ingrese un numero valido.");
+                        }
+
+                        switch (opcion_secundario) {
+                            case 1:
+                                InfoPrestamoPersonal();
+                                break;
+                            case 2:
+                                InfoPrestamoPrendario();
+                                break;
+                            case 3:
+                                InfoPrestamoHipotecario();
+                                break;
+                            case 4:
+                                std::cout << "Volviendo al menu principal... ";
+                                // Salir del bucle del menú secundario y volver al menú principal
+                                break;
+                            default:
+                                std::cout << "Opcion no valida. Intente de nuevo.\n";
+                                break;
+                        }
+
+                        // Si la opción no es 4, salir del bucle del menú secundario y volver al menú principal
+                        if (opcion_secundario == 4) {
+                            break;
+                        }
+                    }
+                    break;
+                }
                 case 2:
-                //Modo_Atencion(cliente);
+                    // Implementa la lógica para Modo_Atencion(cliente);
+                    std::cout << "Implementa la logica para Modo_Atencion(cliente);\n";
+                    break;
                 case 3:
-                std:: cout <<"Saliendo del programa. Gracias por utilizar nuestros servicios.\n";
-                exit(0);
-                default://Por si se escoge algo no valido
-                std:: cout << "Opcion no valida. Intente de Nuevo...\n";   
+                    std::cout << "\nSaliendo del programa. Gracias por utilizar nuestros servicios.\n";
+                    exit(0);
+                default:
+                    std::cout << "Opcion no valida. Intente de Nuevo...\n";
+                    break;
+            }
+            if (opcion != 3) {
+                MostrarMenu();
             }
         } catch (std::exception const& e) {
             std::cerr << e.what() << std::endl;
@@ -40,5 +81,4 @@ void procesarOpcion(Cliente& cliente){
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Descartar la entrada no válida
         }
     } while (opcion != 3);
-
 }
---
+++ src/info.hpp
@@ -0,0 +1,16 @@
+#ifndef INFO_HPP
+#define INFO_HPP
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+// funciones prototipo
+void MostrarMenuSecundario();
+
+void InfoPrestamoPersonal();
+
+void InfoPrestamoPrendario();
+
+void InfoPrestamoHipotecario();
+
+#endif
